fec1f0dcceddfd2cebc8167dd8f651d4765ef810
Rajamanickam <rajamanickam.pr@gmail.com>
2018-04-28 16:37:24 +0530

    Basic commit - initial project

---
+++ Fluxy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fluxy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
